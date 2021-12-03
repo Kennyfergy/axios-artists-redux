17befbc82a73b7abfe65c33ef1d1b4afae4ee853
Matthew Black <matthewallenblack@gmail.com>
2021-12-03 11:09:28 -0600

    convert to prop destructuring, use .map in ArtistList, apply consistent 2-space indentation (some files had two spaces, some had four)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,36 +16,33 @@ function App() {
 
   // Keep this method in App, as it will be used by multiple components
   // You want to keep the code DRY (Don't Repeat Yourself!)
-  // We'll look at another way to handle this with next week's topic Sagas.
-  function refreshArtists() {
+  // We'll look at another way to handle this with next week's topic: Sagas.
+  const refreshArtists = () => {
     axios({
       method: 'GET',
       url: '/artist'
-    }).then( response => {
-      // response.data is the array of artists
-      console.log(response.data);
-      // TODO - update this to dispatch to Redux
-      setArtists(response.data)
-    }).catch( error => {
-      console.log('error on GET', error);
-    });
-  }
+    })
+      .then((response) => {
+        // response.data is the array of artists
+        console.log(response.data);
+        // TODO - update this to dispatch to Redux
+        setArtists(response.data)
+      })
+      .catch((error) => {
+        console.log('error on GET', error);
+      });
+  };
 
- 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Famous Artists</h1>
-        </header>
-        <p>Welcome to our collection of amazing artists!</p>
-        
-        <br/>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Famous Artists</h1>
+      </header>
+      <p>Welcome to our collection of amazing artists!</p>
+      <ArtistList refreshArtists={refreshArtists} artistList={artists} />
+    </div>
+  );
 
-        <ArtistList refreshArtists={refreshArtists} artistList={artists} />
-        
-      </div>
-    );
- 
 }
 
 export default App;
