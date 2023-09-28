ac0b021676c445c89f9cd9c522a6d3ed8a81de70
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-27 23:39:55 -0500

    added Material UI, added some css styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import ArtistList from "../ArtistList/ArtistList";
 import { useDispatch } from "react-redux";
 import ArtistForm from "../ArtistForm/ArtistForm";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
 
 function App() {
   const dispatch = useDispatch();
@@ -36,25 +37,33 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Famous Artists</h1>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Famous Artists</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg">
+          <Typography variant="h4" style={{ marginTop: "2rem" }}>
+            App Intro
+          </Typography>
+
           <nav>
             <Link to="/">Home</Link> |<Link to="/addArtist">Add Artist</Link> |
             <Link to="/allArtists">All Artists</Link>
           </nav>
-        </header>
 
-        <Route exact path="/">
-          <p>Welcome to our collection of amazing artists!</p>
-        </Route>
+          <Route exact path="/">
+            <p>Welcome to our collection of amazing artists!</p>
+          </Route>
 
-        <Route path="/addArtist">
-          <ArtistForm addArtist={addArtist} />
-        </Route>
+          <Route path="/addArtist">
+            <ArtistForm addArtist={addArtist} />
+          </Route>
 
-        <Route path="/allArtists">
-          <ArtistList />
-        </Route>
+          <Route path="/allArtists">
+            <ArtistList />
+          </Route>
+        </Container>
       </div>
     </Router>
   );
